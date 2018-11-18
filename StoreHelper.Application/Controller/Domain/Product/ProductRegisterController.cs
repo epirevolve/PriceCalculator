856ef6ccd260a39702d4ac873215918d0c76fd61
侑宏 井出 <epirevolve@gmail.com>
2018-11-18 23:05:42 +0900

    add register controller

---
+++ StoreHelper.Application/Controller/Domain/Product/ProductRegisterController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfLibrary.ApplicationHelper.Controller;
+
+namespace StoreSupportTool.Application.Controller.Domain.Product
+{
+    sealed class ProductRegisterController : BaseController
+    {
+    }
+}
